6c003e0c09e0f0b5c81fc4f5c389452e62c220ca
f1l1ph <50553234+f1l1ph@users.noreply.github.com>
2025-05-31 01:34:59 +0200

    feat: added utilities for the contract

--- packages/hardhat/contracts/LoanMaster.utilities.sol
+++ packages/hardhat/contracts/LoanMaster.utilities.sol
@@ -1,4 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-contract LoanMasterUtilities {}
+struct LiquidityPoolStruct {
+    uint256 liquidity;
+    mapping(address => uint256) deposits;
+    mapping(address => uint256) depositTimestamps;
+    mapping(address => uint256) borrows;
+    mapping(address => uint256) borrowTimestamps;
+    address tokenAddress;
+    uint256 depositAPR;
+    uint256 borrowAPR;
+}
+
+struct LiquidityPoolSimpleStruct {
+    uint256 liquidity;
+    address tokenAddress;
+    uint256 depositAPR;
+    uint256 borrowAPR;
+}
